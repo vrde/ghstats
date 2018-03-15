cae76f1431327c9842f0d4ac0e6249dd6a668b77
vrde <agranzot@gmail.com>
2018-03-16 00:28:54 +0100

    Add CSV serialization

--- fetch.go
+++ fetch.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/vrde/gitstats/utils"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -19,14 +20,44 @@ type Issues []Issue
 
 type Issue struct {
 	Number      int
-	PullRequest PullRequest `json:"pull_request,omitempty"`
-	CreatedAt   time.Time   `json:"created_at"`
-	UpdatedAt   time.Time   `json:"updated_at"`
-	ClosedAt    time.Time   `json:"closed_at"`
+	PullRequest *PullRequest `json:"pull_request,omitempty"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	ClosedAt    time.Time    `json:"closed_at"`
 }
 
 type PullRequest struct {
 	Url string
+}
+
+// Thanks https://stackoverflow.com/a/33357286/597097
+type CSVAble interface {
+	GetHeaders() []string
+	ToSlice() []string
+}
+
+func (i *Issues) GetHeaders() []string {
+	return []string{"number", "pr_url", "created_at", "updated_at", "closed_at"}
+}
+
+func (i *Issues) ToSlice() [][]string {
+	acc := make([][]string, len(*i))
+	for j, issue := range *i {
+		acc[j] = issue.ToSlice()
+	}
+	return acc
+}
+
+func (i *Issue) GetHeaders() []string {
+	return []string{"number", "pr_url", "created_at", "updated_at", "closed_at"}
+}
+
+func (i *Issue) ToSlice() []string {
+	url := ""
+	if i.PullRequest != nil {
+		url = i.PullRequest.Url
+	}
+	return []string{strconv.Itoa(i.Number), url, i.CreatedAt.String(), i.UpdatedAt.String(), i.ClosedAt.String()}
 }
 
 func fetchIssues(url string, issues *Issues) (error, string) {
@@ -55,7 +86,7 @@ func fetchIssues(url string, issues *Issues) (error, string) {
 	return utils.NextLinkHeader(resp.Header.Get("Link"))
 }
 
-func FetchIssues(repository string, ch chan<- Issues) {
+func FetchIssues(repository string, ch chan<- *Issues) {
 	url := fmt.Sprintf(IssueUrl, repository)
 
 	for {
@@ -69,14 +100,20 @@ func FetchIssues(repository string, ch chan<- Issues) {
 			close(ch)
 			return
 		}
-		ch <- issues
+		ch <- &issues
 	}
 }
 
 func main() {
-	ch := make(chan Issues)
+	w := csv.NewWriter(os.Stdout)
+	ch := make(chan *Issues)
+	h := true
 	go FetchIssues(os.Args[1], ch)
 	for issues := range ch {
-		spew.Dump(issues)
+		if h {
+			w.Write(issues.GetHeaders())
+			h = false
+		}
+		w.WriteAll(issues.ToSlice())
 	}
 }
